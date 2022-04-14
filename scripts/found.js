c80a3ab6c9585169d37a0dc47029919707cf39d9
rahulsingh522003 <85445665+rahulsingh522003@users.noreply.github.com>
2022-04-14 22:28:25 +0530

    changes

--- scripts/found.js
+++ scripts/found.js
@@ -1,12 +1,12 @@
-const submit = async ()=>{
-    
+const submit = async () => {
+
     const Name = document.getElementById('name').value;
     const foundBy = document.getElementById('foundBy').value;
     const Location = document.getElementById('location').value;
     const contact = document.getElementById('contact').value;
     const imageURL = document.getElementById('imageURL').value;
     const content = document.getElementById('content').value;
-    
+
     const data = {
         name: Name,
         foundBy: foundBy,
@@ -15,20 +15,20 @@ const submit = async ()=>{
         imageURL: imageURL,
         content: content
     };
-    
-    fetch('https://bhu-haat-api.herokuapp.com/api/lost',{
+
+    fetch('https://bhu-haat-api.herokuapp.com/api/lost', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-type': 'application/json'
         },
-        body: JSON.stringify(data) 
-        })
-        .then(response=> response.json())
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
         .then(json => console.log(json))
-        .catch(err=> console.log(err));
+        .catch(err => console.log(err));
 }
 
-document.getElementById('submitButton').addEventListener('click', ()=>{
+document.getElementById('submitButton').addEventListener('click', () => {
     submit();
 });
--- scripts/purchase.js
+++ scripts/purchase.js
@@ -1,17 +1,17 @@
 const showItem = (Item) => {
-    const div=document.createElement('div');
+    const div = document.createElement('div');
     div.classList.add('item');
-    const img=document.createElement('img');
-    img.src=Item.imageURL;
+    const img = document.createElement('img');
+    img.src = Item.imageURL;
     img.classList.add('imageContainer');
     div.appendChild(img);
     props = ['name', 'owner', 'cost', 'contact'];
     propsTitle = ['Name', 'Owner', 'Cost', 'Contact'];
     for (let i = 0; i < props.length; i++) {
-        div.innerHTML+=`<p>${propsTitle[i]}: ${Item[props[i]]}<\p>`;
-        div.innerHTML+='<br>'
+        div.innerHTML += `<p>${propsTitle[i]}: ${Item[props[i]]}<\p>`;
+        div.innerHTML += '<br>'
     };
-    const container=document.getElementById('container');
+    const container = document.getElementById('container');
     container.appendChild(div);
 }
 
@@ -19,32 +19,32 @@ const fetchLostItems = () => {
     fetch('http://bhu-haat-api.herokuapp.com/api/sell')
         .then(response => response.json())
         .then(data => data.map(Item => {
-                showItem(Item);
-            })
+            showItem(Item);
+        })
         )
 }
 
 fetchLostItems();
 
-const search = (val)=>{
-    api='http://bhu-haat-api.herokuapp.com/api/sell';
-    if (val!='') {
-        api+=`?name=${val}`
+const search = (val) => {
+    api = 'http://bhu-haat-api.herokuapp.com/api/sell';
+    if (val != '') {
+        api += `?name=${val}`
     }
-    document.getElementById('container').innerHTML='';
+    document.getElementById('container').innerHTML = '';
     fetch(api)
-    .then(response => response.json())
-    .then(data => data.map(Item => {
+        .then(response => response.json())
+        .then(data => data.map(Item => {
             showItem(Item);
         }));
 }
 
-document.getElementById('searchButton').addEventListener('click', ()=>{
+document.getElementById('searchButton').addEventListener('click', () => {
     search(document.getElementById('searchBar').value)
-} );
+});
 
-document.addEventListener('keydown', (event)=>{
-    if (event.key=='Enter') {
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'Enter') {
         document.getElementById('searchButton').click();
     }
 })
