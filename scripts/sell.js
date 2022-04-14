3ec7cd3e31e72754f897d06b5fa038e09ab2bf1d
Nikhil Rana <nikhil.student.eee20@itbhu.ac.in>
2022-04-14 22:21:42 +0530

    Update Changes

--- scripts/sell.js
+++ scripts/sell.js
@@ -1,11 +1,11 @@
-const submit = async ()=>{
-    
+const submit = async () => {
+
     const Name = document.getElementById('name').value;
     const owner = document.getElementById('owner').value;
     const cost = document.getElementById('cost').value;
     const contact = document.getElementById('contact').value;
     const imageURL = document.getElementById('imageURL').value;
-    
+
     const data = {
         name: Name,
         owner: owner,
@@ -13,20 +13,26 @@ const submit = async ()=>{
         contact: contact,
         imageURL: imageURL,
     };
-    
-    fetch('https://bhu-haat-api.herokuapp.com/api/sell',{
+
+    fetch('https://bhu-haat-api.herokuapp.com/api/sell', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-type': 'application/json'
         },
-        body: JSON.stringify(data) 
-        })
-        .then(response=> response.json())
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
         .then(json => console.log(json))
-        .catch(err=> console.log(err));
+        .catch(err => console.log(err));
 }
 
-document.getElementById('submitButton').addEventListener('click', ()=>{
+document.getElementById('submitButton').addEventListener('click', () => {
     submit();
 });
+
+document.getElementById('close').addEventListener('click', () => {
+    setTimeout(() => {
+        location.reload();
+    }, 500);
+})
